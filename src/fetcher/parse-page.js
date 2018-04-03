a214b756d262da7b116a84160c0c50a47b1a398f
Kennedy Rose <kennedy@kennedyrose.com>
2018-04-03 16:05:29 -0500

    Parse links

--- src/fetcher/parse-page.js
+++ src/fetcher/parse-page.js
@@ -6,14 +6,23 @@ function parsePage(text) {
 	let links = []
 	let linkEls = doc.querySelectorAll('a')
 	for (let i = 0; i < linkEls.length; i++) {
-		let href = linkEls[i].href
-		let str = linkEls[i].getAttribute('href')
+		let href = linkEls[i].getAttribute('href')
+		if(!href || href.charAt(0) === '#') continue
+		if(href.indexOf('http') === 0){
+			if (href.indexOf(this.options.origin) !== 0){
+				continue
+			}
+		}
+		else{
+			if(href.charAt(0) === '/'){
+				href = href.substring(1)
+			}
+			href = `${this.options.origin}${href}`
+		}
 		if (
-			href &&
-			str.charAt(0) !== '#' &&
-			href.indexOf(this.origin) === 0 &&
 			this.fetchedUrls.indexOf(href) === -1 &&
-			this.urls.indexOf(href) === -1
+			this.urls.indexOf(href) === -1 &&
+			links.indexOf(href) === -1
 		) {
 			links.push(href)
 		}
@@ -25,16 +34,22 @@ function parsePage(text) {
 		title = titleEl.textContent
 	}
 
-	let contentEl = doc.querySelector(this.options.content)
-	let content = ''
-	if (contentEl) {
-		content = contentEl.textContent.replace(regSpace, ' ')
-	}
-
 	let descriptionEl = doc.querySelector(this.options.description)
 	let description = ''
 	if (descriptionEl){
 		description = descriptionEl.getAttribute('content') || descriptionEl.textContent
+	}
+
+	let contentEl = doc.querySelector(this.options.content)
+	let content = ''
+	if (contentEl) {
+		// Remove unwanted tags
+		let tags = contentEl.querySelectorAll('script')
+		for (let i = tags.length; i--;) {
+			let tag = tags[i]
+			tag.parentElement.removeChild(tag)
+		}
+		content = contentEl.textContent.replace(regSpace, ' ')
 	}
 
 	return {
